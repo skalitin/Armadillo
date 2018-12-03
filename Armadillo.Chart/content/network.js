7a1276faf387a9370cfd7220da830e4054864318
Sergey Kalitin <sergey.kalitin@gmail.com>
2018-12-03 13:37:51 +0300

    Adding vis.js in progress

---
+++ Armadillo.Chart/content/network.js
@@ -0,0 +1,26 @@
+function RenderNetwork(ignore) {
+    var nodes = new vis.DataSet([
+        {id: 1, label: 'Node 1'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+        {id: 4, label: 'Node 4'},
+        {id: 5, label: 'Node 5'}
+    ]);
+
+    var edges = new vis.DataSet([
+        {from: 1, to: 3},
+        {from: 1, to: 2},
+        {from: 2, to: 4},
+        {from: 2, to: 5}
+    ]);
+
+    var container = document.getElementById('my-component');
+
+    var data = {
+        nodes: nodes,
+        edges: edges
+    };
+    var options = {};
+
+    var network = new vis.Network(container, data, options);
+}
