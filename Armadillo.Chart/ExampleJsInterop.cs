d43e5f88d0252fa28cd76e20d8f3b22ea00ccd97
Sergey Kalitin <sergey.kalitin@gmail.com>
2018-12-03 13:49:11 +0300

    work in progress

--- Armadillo.Chart/ExampleJsInterop.cs
+++ Armadillo.Chart/ExampleJsInterop.cs
@@ -5,6 +5,11 @@ namespace Armadillo.Chart
 {
     public class ExampleJsInterop
     {
+        // public static Task RenderNetwork()
+        // {
+        //     return JSRuntime.Current.InvokeAsync<string>("RenderNetwork");
+        // } 
+
         public static Task<string> Prompt(string message)
         {
             // Implemented in exampleJsInterop.js
