83a97651fc273f2d6564fe9b477771727771e932
Sergey Kalitin <sergey.kalitin@gmail.com>
2018-12-04 12:34:59 +0300

    Load subcase datafrom server

---
+++ Armadillo.Server/Controllers/SubcasesController.cs
@@ -0,0 +1,28 @@
+﻿using Armadillo.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Armadillo.Server.Controllers
+{
+    [Route("api")]
+    public class SubcasesController : Controller
+    {
+        [HttpGet("[action]")]
+        public IEnumerable<Subcase> Subcases()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Subcase
+            {
+                Id = String.Format("{0}", index),
+                Title = "Test title",
+                // Level = rng.Next(5),
+                Customer = "",
+                Owner = "",
+                Status = ""
+            });
+        }
+    }
+}
---
+++ Armadillo.Shared/Subcase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Armadillo.Shared
+{
+    public class Subcase
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Customer { get; set; }
+        public string Owner { get; set; }
+        public string Status { get; set; }
+        // public int Level { get; set; }
+    }
+}
