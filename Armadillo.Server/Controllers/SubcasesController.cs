288c8f19915604c273542bc1b56d14019aebd222
Sergey Kalitin <sergey.kalitin@gmail.com>
2018-12-08 20:40:20 +0300

    Lorem ipsum

--- Armadillo.Server/Controllers/SubcasesController.cs
+++ Armadillo.Server/Controllers/SubcasesController.cs
@@ -26,8 +26,8 @@ namespace Armadillo.Server.Controllers
             var max = rng.Next(10, 15);
             return Enumerable.Range(1, max).Select(index => new Subcase
             {
-                Id = String.Format("{0}", index),
-                Title = "Title " + rng.Next(100),
+                Id = String.Format("{0}-1", rng.Next(405000, 405999)),
+                Title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit " + rng.Next(100),
                 Level = "" + rng.Next(1, 4),
                 Customer = "Customer " + rng.Next(1, 5),
                 Owner = "Owner " + rng.Next(1, 7),
