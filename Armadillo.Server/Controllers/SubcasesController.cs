c20ee49c75baccac9649348f3bea87eb28d7340e
Sergey Kalitin (skalitin) <sergey.kalitin@gmail.com>
2018-12-10 17:45:11 +0300

    Show real data - draft

--- Armadillo.Server/Controllers/SubcasesController.cs
+++ Armadillo.Server/Controllers/SubcasesController.cs
@@ -1,4 +1,5 @@
 ﻿using Armadillo.Shared;
+using Armadillo.Siebel;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -19,20 +20,23 @@ namespace Armadillo.Server.Controllers
                 "Migration Manager for AD"
             };
         }
+        
         [HttpGet("subcases")]
         public Product Subcases(string productName)
         {
-            var rng = new Random();
-            var max = rng.Next(10, 15);
-            var subcases = Enumerable.Range(1, max).Select(index => new Subcase
-            {
-                Id = String.Format("{0}-1", rng.Next(405000, 405999)),
-                Title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit " + rng.Next(100),
-                Level = "" + rng.Next(1, 4),
-                Customer = "Customer " + rng.Next(1, 5),
-                Owner = "Owner " + rng.Next(1, 7),
-                Status = "Status " + rng.Next(1, 3),
-            });
+            var subcases = new ReportServerAccessor().GetSubcasesAsync().Result;
+
+            // var rng = new Random();
+            // var max = rng.Next(10, 15);
+            // var subcases = Enumerable.Range(1, max).Select(index => new Subcase
+            // {
+            //     Id = String.Format("{0}-1", rng.Next(405000, 405999)),
+            //     Title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit " + rng.Next(100),
+            //     Level = "" + rng.Next(1, 4),
+            //     Customer = "Customer " + rng.Next(1, 5),
+            //     Owner = "Owner " + rng.Next(1, 7),
+            //     Status = "Status " + rng.Next(1, 3),
+            // });
             
             productName = String.IsNullOrEmpty(productName) ? "RMAD" : productName;
             return new Product()
--- Armadillo.Siebel/Program.cs
+++ Armadillo.Siebel/Program.cs
@@ -2,51 +2,93 @@
 using System.Net.Http;
 using System.Net;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using HtmlAgilityPack;
+using Armadillo.Shared;
 
 namespace Armadillo.Siebel
 {
-    class Program
+    public class ReportServerAccessor
     {
-        static void Main(string[] args)
-        {
-            var url = @"http://tfsreports.prod.quest.corp/ReportServer?/Siebel/SPB/SLA+Siebel+(SPb)&rs:Command=Render&Location=EMEA-RU-St.%20Petersburg&Products=Recovery%20Manager%20for%20AD&rs:Format=HTML4.0&rc:LinkTarget=_top&rc:Javascript=false&rc:Toolbar=false";
-            // var url = @"https://tfsreports.webapps.quest.com/ReportServer?/Siebel/SPB/SLA+Siebel+(SPb)&rs:Command=Render&Location=EMEA-RU-St.%20Petersburg&Products=Recovery%20Manager%20for%20AD&rs:Format=HTML4.0&rc:LinkTarget=_top&rc:Javascript=false&rc:Toolbar=false";
+        private string url_;
 
+        public ReportServerAccessor(string url = null)
+        {
+            //  url = @"https://tfsreports.webapps.quest.com/ReportServer?/Siebel/SPB/SLA+Siebel+(SPb)&rs:Command=Render&Location=EMEA-RU-St.%20Petersburg&Products=Recovery%20Manager%20for%20AD&rs:Format=HTML4.0&rc:LinkTarget=_top&rc:Javascript=false&rc:Toolbar=false";
+            url_ = @"http://tfsreports.prod.quest.corp/ReportServer?/Siebel/SPB/SLA+Siebel+(SPb)&rs:Command=Render&Location=EMEA-RU-St.%20Petersburg&Products=Recovery%20Manager%20for%20AD&rs:Format=HTML4.0&rc:LinkTarget=_top&rc:Javascript=false&rc:Toolbar=false";
+            //url_ = url;
+        }
+
+        public async Task<IEnumerable<Subcase>> GetSubcasesAsync()
+        {
             var page = "";
             try
             {
-                page = GetPageAsync(url).Result;
-            } catch (Exception ex)
+                page = await GetPageAsync(url_);
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
-                return;
+                return new Subcase[]{};
             }
-            // Console.WriteLine(page);
-            
+                        
             var htmlDoc = new HtmlDocument();
             htmlDoc.LoadHtml(page);
+
             var htmlBody = htmlDoc.DocumentNode;
-            var test = htmlBody.SelectSingleNode("//table[@class='a209']");
-            //var test = htmlBody.SelectSingleNode("//table");
+            var tableNode = htmlBody.SelectSingleNode("//table[@class='a209']");
+            var rowNodes = tableNode.SelectNodes("tr");
+                        
+            Console.WriteLine("Rows: {0}", rowNodes.Count);
+            var subcases = new List<Subcase>();
+            int i = 0;
+            foreach(var node in rowNodes)
+            {
+                i++;
+                if(i <= 2) continue;
+                var cells = node.SelectNodes("td");
+                var subcase = new Subcase()
+                {
+                    Id = cells[1].InnerText,
+                    Title = cells[2].InnerText,
+                    //Level = cells[3].InnerText,
+                    Level = "3",
+                    Owner = cells[4].InnerText,
+                    Status = cells[7].InnerText,
+                    Customer = cells[9].InnerText
+                };
+                
+                subcases.Add(subcase);
+            }
 
-            Console.WriteLine(test.OuterHtml);
-
-            // HtmlWeb web = new HtmlWeb();
-            // var htmlDoc = web.Load(html);
-            // var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
-            // Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
+            return subcases;
         }
 
-        static async Task<string> GetPageAsync(string url)
+        private async Task<string> GetPageAsync(string url)
         {
             var uri = new Uri(url);
             var credentialsCache = new CredentialCache { { uri, "NTLM", CredentialCache.DefaultNetworkCredentials } };
             var handler = new HttpClientHandler { Credentials = credentialsCache };
+            
+            Console.WriteLine("Sending request...");
             using(var httpClient = new HttpClient(handler) { BaseAddress = uri  })
             {
                 return await httpClient.GetStringAsync(url);
             }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subcases = new ReportServerAccessor().GetSubcasesAsync().Result;
+            foreach(var subcase in subcases) 
+            {
+                Console.WriteLine("{0} {1}", subcase.Id, subcase.Title);
+            }
+
+           
         }
     }
 
