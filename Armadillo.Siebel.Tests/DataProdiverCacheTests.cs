acb0629a6610bf7ae031f95b2709c6495e1dcffa
Sergey Kalitin <sergey.kalitin@gmail.com>
2018-12-24 08:50:13 +0300

    Test rename

---
+++ Armadillo.Siebel.Tests/DataProdiverCacheTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Armadillo.Shared;
+
+namespace Armadillo.Siebel.Tests
+{
+    class TestDataProvider : ISubcaseDataProdiver
+    {
+        public IEnumerable<string> GetProducts()
+        {
+            return new string[]{};
+        }
+
+        public Task<IEnumerable<Subcase>> GetSubcasesAsync(string product)
+        {
+            return Task<IEnumerable<Subcase>>.Run(() =>
+            {
+                return (IEnumerable<Subcase>)new List<Subcase>();
+            });
+        }
+    }
+
+    [TestClass]
+    public class DataProdiverCacheTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
